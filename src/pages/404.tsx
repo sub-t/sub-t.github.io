d132b24eac499468a336822dd58f86581f06ff38
sub-t <subt@outlook.jp>
2022-07-12 06:22:43 +0900

    [fix] 404ページの修正

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,23 +1,21 @@
 import { Link } from '@/components/material/Link';
 
 const View = () => (
-  <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
-    <div className="vstack items-center p-8 bg-primary-1">
-      <p className="mb-4 text-sm md:text-base font-bold uppercase text-accent-1">
-        That’s a 404
-      </p>
-      <h1 className="mb-2 text-2xl md:text-3xl font-bold text-primary-1">
-        Page not found
-      </h1>
+  <div className="vstack items-center p-8 bg-primary-1">
+    <p className="mb-4 text-sm md:text-base font-bold uppercase text-accent-1">
+      That’s a 404
+    </p>
+    <h1 className="mb-2 text-2xl md:text-3xl font-bold text-primary-1">
+      Page not found
+    </h1>
 
-      <p className="mb-12 text-gray-500 dark:text-gray-300 md:text-lg">
-        The page you’re looking for doesn’t exist.
-      </p>
+    <p className="mb-12 text-gray-500 dark:text-gray-300 md:text-lg">
+      The page you’re looking for doesn’t exist.
+    </p>
 
-      <Link href="/" passHref>
-        <a className="py-2 px-6 btn">Go home</a>
-      </Link>
-    </div>
+    <Link href="/" passHref>
+      <a className="py-2 px-6 btn">Go home</a>
+    </Link>
   </div>
 );
 
