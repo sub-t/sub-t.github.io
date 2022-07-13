178d7c2e5ca6e8596e75486496b29f953eb0ee20
sub-t <subt@outlook.jp>
2022-07-13 21:27:22 +0900

    [update] NextSEOの移動

--- src/components/pages/posts/index.tsx
+++ src/components/pages/posts/index.tsx
@@ -14,51 +14,54 @@ export const Posts: React.VFC<Props> = ({ post }) => {
   const lg = useBreakPoint('lg');
 
   return (
-    <Contents
-      main={
-        <article>
-          <NextSeo
-            title={post.title}
-            description={post.excerpt}
-            openGraph={{
-              url: process.env.NEXT_PUBLIC_ROOT_URL,
-              title: post.title,
-              description: post.excerpt,
-              images: [
-                {
-                  url: post.ogImage.url,
-                },
-              ],
-            }}
-          />
-          <Post post={post} />
-        </article>
-      }
-      aside={
-        <div className="vstack gap-6 h-full">
-          <Profile />
-          {lg && (
-            <div className="sticky top-20">
-              <Toc />
-            </div>
-          )}
-        </div>
-      }
-      hamburgerMenu={
-        <div
-          role="button"
-          tabIndex={0}
-          onClick={() =>
-            document.dispatchEvent(
-              new KeyboardEvent('keydown', { key: 'Escape' }),
-            )
-          }
-          onKeyDown={() => {}}
-          className="overflow-y-auto cursor-default"
-        >
-          <Toc />
-        </div>
-      }
-    />
+    <>
+      <NextSeo
+        title={post.title}
+        description={post.excerpt}
+        openGraph={{
+          url: process.env.NEXT_PUBLIC_ROOT_URL,
+          title: post.title,
+          description: post.excerpt,
+          images: [
+            {
+              url: post.ogImage.url,
+            },
+          ],
+        }}
+      />
+
+      <Contents
+        main={
+          <article>
+            <Post post={post} />
+          </article>
+        }
+        aside={
+          <div className="vstack gap-6 h-full">
+            <Profile />
+            {lg && (
+              <div className="sticky top-20">
+                <Toc />
+              </div>
+            )}
+          </div>
+        }
+        hamburgerMenu={
+          <div
+            role="button"
+            tabIndex={0}
+            onClick={() =>
+              document.dispatchEvent(
+                new KeyboardEvent('keydown', { key: 'Escape' }),
+              )
+            }
+            onKeyDown={() => {}}
+            className="overflow-y-auto cursor-default"
+          >
+            <Toc />
+          </div>
+        }
+      />
+    </>
   );
 };
