612c0fa7222359557b40ff951a446203ddce6812
sub-t <subt@outlook.jp>
2022-07-15 17:39:20 +0900

    [fix] aboutページの修正

--- src/components/pages/about/index.tsx
+++ src/components/pages/about/index.tsx
@@ -1,4 +1,6 @@
 import { Contents } from '@/components/features/app/Contents';
 import { Profile } from '@/components/features/app/Profile';
 
-export const About = () => <Contents main={<Profile />} />;
+export const About = () => (
+  <Contents className="lg:block" main={<Profile />} />
+);
